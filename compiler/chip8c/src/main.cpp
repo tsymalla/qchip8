cc7a8a5c6d897c671eaac9056285803251692679
Thomas Symalla <info@seuchomat.org>
2021-07-02 00:05:50 +0200

    Added compiler entry point executable.

---
+++ compiler/chip8c/src/main.cpp
@@ -0,0 +1,31 @@
+#include <lexer.hpp>
+#include <parser.hpp>
+#include <fstream>
+#include <string>
+
+using namespace compiler;
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "Usage: chip8c filename";
+		return 1;
+	}
+
+	std::ifstream inputFile{ argv[1], std::ios_base::in };
+	if (!inputFile.is_open())
+	{
+		std::cerr << "Could not open input file \"" << argv[1] << ".\n";
+		return 1;
+	}
+
+	std::string inputBuffer{ std::istreambuf_iterator<char>(inputFile), std::istreambuf_iterator<char>() };
+	
+	Lexer lexer(std::move(inputBuffer));
+	const auto& tokens = lexer.Lex();
+	Parser parser(tokens);
+	parser.Parse();
+
+	return 0;
+}
